343ed4bb9d408bd162b51532345fb4f6fc7adc9c
Egor <GladkihEgor@icloud.com>
2021-01-17 00:44:53 +0500

    Добавил подсчет распознаных людей

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ face_cascade_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcas
 video_capture = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 video_writer = cv2.VideoWriter('output/FaceRecognition.avi', fourcc, 10.0, (1280, 1024))
-controlPoints = [30, 48, 54, 36, 45, 39, 42, 62]
+
+FACE_RECOGNITION = 'Face recognition'
+ON = 0
+OFF = 1
+show_rectangle = 'Show rectangle'
+show_dots = 'Show_dots'
+count = 'Dots count'
+control_points = [30, 48, 54, 36, 45, 39, 42, 62]
+faces_count = []
 
 
-def nothing(self):
+def nothing(*args):
     pass
 
 
 def choose_dots(flag):
     if flag == 0:
-        return controlPoints
+        return control_points
     elif flag == 1:
         return range(17, 27)
     elif flag == 2:
@@ -29,37 +37,35 @@ def choose_dots(flag):
         return range(0, 68)
 
 
-
-FACE_RECOGNITION = 'Face recognition'
 cv2.namedWindow(FACE_RECOGNITION)
-ON = 0
-OFF = 1
-show_rectangle = 'Show rectangle'
-show_name = 'Show_name'
-show_dots = 'Show_dots'
-count = 'Dots count'
 cv2.createTrackbar(count, FACE_RECOGNITION, 0, 5, nothing)
-cv2.createTrackbar(show_dots, FACE_RECOGNITION, ON, OFF, nothing)
-cv2.createTrackbar(show_name, FACE_RECOGNITION, ON, OFF, nothing)
 cv2.createTrackbar(show_rectangle, FACE_RECOGNITION, ON, OFF, nothing)
+cv2.createTrackbar(show_dots, FACE_RECOGNITION, ON, OFF, nothing)
 
 while True:
     is_show_rectangle = cv2.getTrackbarPos(show_rectangle, FACE_RECOGNITION)
-    is_show_name = cv2.getTrackbarPos(show_name, FACE_RECOGNITION)
-    is_show_dots = cv2.getTrackbarPos(show_dots, FACE_RECOGNITION)
     dots_count = cv2.getTrackbarPos(count, FACE_RECOGNITION)
+    is_show_dots = cv2.getTrackbarPos(show_dots, FACE_RECOGNITION)
     is_success, image = video_capture.read()
     if is_success:
         image = cv2.flip(image, 1)
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade_classifier.detectMultiScale(image_gray, 1.9, 5)
+        face = face_cascade_classifier.detectMultiScale(image_gray, 1.9, 5)
+        cv2.putText(img=image,
+                    text=f'Number of recognized people: {len(faces_count)}',
+                    org=(10, 50),
+                    fontFace=cv2.FONT_HERSHEY_DUPLEX,
+                    fontScale=1,
+                    color=(255, 255, 255))
+        faces_count.clear()
 
-        for (x, y, w, h) in faces:
+        for (x, y, w, h) in face:
             left = x
             top = y
             right = (x + w)
             bottom = (y + h)
             face = dlib.rectangle(left, top, right, bottom)
+            faces_count.append(face)
             if is_show_rectangle == ON:
                 cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
 
