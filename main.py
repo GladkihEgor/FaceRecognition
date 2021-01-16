544c79d29e24e4e7212e353fbf079a7382284dc1
Egor <GladkihEgor@icloud.com>
2021-01-16 16:55:38 +0500

    Вернул каскады Хаара

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import cv2
 import dlib
 
-detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('./resources/shape_predictor_68_face_landmarks.dat')
+faceCascadeClassifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 videoCapture = cv2.VideoCapture(0)
 
 while True:
-    success, image = videoCapture.read()
-    if success:
+    isSuccess, image = videoCapture.read()
+    if isSuccess:
         imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = faceCascadeClassifier.detectMultiScale(imageGray, 1.9, 3)
 
-        faces = detector(imageGray)
-        for face in faces:
-            x1 = face.left()
-            y1 = face.top()
-            x2 = face.right()
-            y2 = face.bottom()
-            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        for (x, y, w, h) in faces:
+            left = x
+            top = y
+            right = (x + w)
+            bottom = (y + h)
+            face = dlib.rectangle(left, top, right, bottom)
+            cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
 
             landmarks = predictor(imageGray, face)
             for n in range(0, 68):
@@ -26,7 +27,7 @@ while True:
                 cv2.circle(image, (x, y), 3, (255, 0, 0), -1)
 
         cv2.imshow('Done', image)
-        if cv2.waitKey(1) & 0xff == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         print('Не удалось считать изображение с камеры')
